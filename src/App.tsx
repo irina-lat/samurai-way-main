3b56525913dd947527f0d5a4d3d16e48ee06e7bf
irina-lat <irochka.bars@mail.ru>
2022-06-29 02:52:55 +0300

    TS header and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,35 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Dialogs from "./components/Dialogs/Dialogs";
 import Profile from "./components/Profile/Profile";
+// import Dialogs from "./components/Dialogs/Dialogs";
+
+
+
+const headerDate={
+    logo:'https://kartinkin.net/uploads/posts/2021-01/1610501112_2-p-kruglii-fon-dlya-logotipa-3.png'
+}
+
+const navbarDate=[
+    {title:"Propfile"},
+    {title:"Message"},
+    {title:"News"},
+    {title:"Music"},
+    {title:"Settings"}
+]
 
 
 function App() {
-
         return (
             <div className="app-wrapper">
-                <Header/>
-                <Navbar/>
+                <Header logo={headerDate.logo} />
+                <Navbar navbarDate={navbarDate} />
                 <div className='app-wrapper-content'>
-                    <Dialogs/>
                     <Profile/>
+                    {/*<Dialogs/>*/}
+
                 </div>
+
             </div>
         )
     }
