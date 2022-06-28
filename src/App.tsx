e26186470acc19ab97b09ebe91e4a8798e20398a
irina-lat <irochka.bars@mail.ru>
2022-06-28 21:28:21 +0300

    base for work

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Dialogs from "./components/Dialogs/Dialogs";
+import Profile from "./components/Profile/Profile";
+
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
-}
 
-export default App;
+        return (
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Dialogs/>
+                    <Profile/>
+                </div>
+            </div>
+        )
+    }
+
+    export default App;
